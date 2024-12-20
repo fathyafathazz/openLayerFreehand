76858c86221af865fbddd4b5c5601c6fcb35c192
fathyafathazz <fathyafathazzra@gmail.com>
2024-12-20 11:24:53 +0700

    first

---
+++ main.js
@@ -0,0 +1,49 @@
+import Draw from 'https://cdn.skypack.dev/ol/interaction/Draw.js';
+import Map from 'https://cdn.skypack.dev/ol/Map.js';
+import View from 'https://cdn.skypack.dev/ol/View.js';
+import {OSM, Vector as VectorSource} from 'https://cdn.skypack.dev/ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'https://cdn.skypack.dev/ol/layer.js';
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const vector = new VectorLayer({
+  source: source,
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+});
+
+const typeSelect = document.getElementById('type');
+
+let draw; // global so we can remove it later
+function addInteraction() {
+  const value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new Draw({
+      source: source,
+      type: typeSelect.value,
+      freehand: true,
+    });
+    map.addInteraction(draw);
+  }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+addInteraction();
